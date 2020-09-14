2d182a64729a4a51faa2d13defda43bd343a5e10
Martin Ransbach <ransbachm@gmail.com>
2020-09-14 15:37:51 +0200

    Update main.cpp

--- II/15_inheritance/Chronos2/main.cpp
+++ II/15_inheritance/Chronos2/main.cpp
@@ -2,13 +2,14 @@
 #include "base_impl.hpp"
 
 int main() {
-    using namespace chronos2;
     using std::cout;
+    using namespace chronos2;
 
     day d {12};
     date date {2020};
 
-    cout << d << "\n";
+    d.print(cout);
+    cout << "\n";
     date.print(cout) << "\n";
     cout << date;
 
