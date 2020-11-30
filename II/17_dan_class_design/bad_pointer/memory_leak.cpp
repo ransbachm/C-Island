e2610f083547b42d66814ecb86fd43d5e3fa595c
Martin Ransbach <ransbachm@gmail.com>
2020-11-30 18:00:52 +0100

    Clean up

--- II/17_dan_class_design/bad_pointer/memory_leak.cpp
+++ II/17_dan_class_design/bad_pointer/memory_leak.cpp
@@ -10,40 +10,38 @@
 using std::string; using std::cout;
 
 class bi_value_store {
-    /*
+    
     value* firstVal_; // raw pointer -> bad
     value* secondVal_;
-    */
+    
 
-   std::unique_ptr<value> firstVal_;
-   std::unique_ptr<value> secondVal_;
+    // how to actually do this
+   /*std::unique_ptr<value> firstVal_;
+   std::unique_ptr<value> secondVal_;*/
 
 public:
     // new value works because of implicit type conversion in constructor of unique_ptr
     bi_value_store(string first, string second) : firstVal_{new value}, secondVal_{new value} {}
 
     const value* getFirst() const {
-        return firstVal_.get();
+        return firstVal_;
+        //return firstVal_.get();
     }
 
 
 };
 
 void leak() {
-    // this would leak memory
-    try {
-        //cout << "Now creating s1\n";
-        auto s1 = bi_value_store{"ABC", "DEF"};
-        //cout << "Now creating s1\n";
-        auto s2 = bi_value_store{"GHI", "JKL"};
-        //cout << "Done creating s1 and s2!\n"; // should not be called
-    } catch (std::runtime_error err) {
-        //cout << "There was an error!\n";
-    }
+    // this will / would leak memory
+    //cout << "Now creating s1\n";
+    auto s1 = bi_value_store{"ABC", "DEF"};
+    //cout << "Now creating s1\n";
+    auto s2 = bi_value_store{"GHI", "JKL"};
     //cout << "Done.\n";
 }
 
 int main() {
+    cout << "Starting.\n";
     while(true) leak();
 }
 
