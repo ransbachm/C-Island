6e54017936cb1d733078e8dd5d21d196da13be74
Martin Ransbach <ransbachm@gmail.com>
2020-12-30 03:17:39 +0100

    Expand with C syntax example

--- III/23_Templates/Function_parameter/Basics/function_parameter.cpp
+++ III/23_Templates/Function_parameter/Basics/function_parameter.cpp
@@ -8,10 +8,22 @@ int calc(int a, int b, std::function<int(int, int)> op) {
     return op(a,b);
 }
 
+// c style syntax, implicitly converted to above
+int calc_c(int a, int b, int(*op)(int, int)) {
+    return op(a, b);
+}
+
 int plus(int a, int b) {return a+b;}
 int mult(int a, int b) {return a*b;}
 
 int main() {
     cout << calc(3,5, plus) << "\n";
     cout << calc(3,5, mult) << "\n";
+
+    cout << calc_c(3,5, &plus) << "\n"; // not needed because converted to c++ style anyway
+    cout << calc_c(3,5, &mult) << "\n";
+
+    cout << calc_c(3,5, plus) << "\n";
+    cout << calc_c(3,5, mult) << "\n";
+
 }
